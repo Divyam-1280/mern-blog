bc9ebd5423aa82580bab966451de93043bc0d732
Divyam-1280 <divyamraj700@gmail.com>
2024-06-26 23:38:59 +0530

    signup api route completed

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import connectDB from "../config/mongoDBconnection.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
